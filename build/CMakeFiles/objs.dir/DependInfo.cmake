a5196c84a921c9e3e451944648a5d1a97aed5d87
SimonLundell <simonlundell@hotmail.com>
2022-03-13 14:30:27 +0100

    CMAKE added

---
+++ build/CMakeFiles/objs.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/simon/rt_plotting/resources/imgui.cpp" "CMakeFiles/objs.dir/resources/imgui.cpp.o" "gcc" "CMakeFiles/objs.dir/resources/imgui.cpp.o.d"
+  "/home/simon/rt_plotting/resources/imgui_draw.cpp" "CMakeFiles/objs.dir/resources/imgui_draw.cpp.o" "gcc" "CMakeFiles/objs.dir/resources/imgui_draw.cpp.o.d"
+  "/home/simon/rt_plotting/resources/imgui_impl_opengl3.cpp" "CMakeFiles/objs.dir/resources/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/objs.dir/resources/imgui_impl_opengl3.cpp.o.d"
+  "/home/simon/rt_plotting/resources/imgui_impl_sdl.cpp" "CMakeFiles/objs.dir/resources/imgui_impl_sdl.cpp.o" "gcc" "CMakeFiles/objs.dir/resources/imgui_impl_sdl.cpp.o.d"
+  "/home/simon/rt_plotting/resources/imgui_tables.cpp" "CMakeFiles/objs.dir/resources/imgui_tables.cpp.o" "gcc" "CMakeFiles/objs.dir/resources/imgui_tables.cpp.o.d"
+  "/home/simon/rt_plotting/resources/imgui_widgets.cpp" "CMakeFiles/objs.dir/resources/imgui_widgets.cpp.o" "gcc" "CMakeFiles/objs.dir/resources/imgui_widgets.cpp.o.d"
+  "/home/simon/rt_plotting/resources/implot.cpp" "CMakeFiles/objs.dir/resources/implot.cpp.o" "gcc" "CMakeFiles/objs.dir/resources/implot.cpp.o.d"
+  "/home/simon/rt_plotting/resources/implot_items.cpp" "CMakeFiles/objs.dir/resources/implot_items.cpp.o" "gcc" "CMakeFiles/objs.dir/resources/implot_items.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
